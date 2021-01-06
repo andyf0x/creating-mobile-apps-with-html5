b3301eb00a3313ab1654a794a229f010c8bc8235
Andy Fox <andyf0x@users.noreply.github.com>
2021-01-05 22:07:56 -0800

    Add StandardJS to tidy up JS code

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,63 +1,61 @@
+'use strict'
+
 const TECHNO_SPEED = 2
 
-var light = new Array();
-var t;
-var color = 0;
-var speed;
+const light = []
+const intervals = []
 
-light[0] = 'black';
-light[1] = 'white';
-light[2] = 'red';
-light[3] = "blue";
-light[4] = "green";
-light[5] = "orange";
+light[0] = 'black'
+light[1] = 'white'
+light[2] = 'red'
+light[3] = 'blue'
+light[4] = 'green'
+light[5] = 'orange'
 
-function flip(whichWay) {
-  document.body.style.backgroundColor = light[whichWay];
+// eslint-disable-next-line no-unused-vars
+function flip (whichWay) {
+  document.body.style.backgroundColor = light[whichWay]
   document.getElementById('color').innerText = light[whichWay]
-  stopFlip();
+  stopFlip()
 }
 
-function autoFlip() {
-  document.body.style.backgroundColor = light[color];
-  document.getElementById('color').innerText = light[color]
-  if (color > 1) {
-    color -= 2;
-  } else {
-    color = light.length - 1;
+function autoFlip () {
+  let idx = light.indexOf(document.body.style.backgroundColor)
+
+  if (idx === light.length - 1) {
+    idx = -1
   }
+  document.body.style.backgroundColor = light[idx + 1]
+  document.getElementById('color').innerText = document.body.style.backgroundColor
 }
 
-function doAutoFlip(fps) {
-  if (fps == 0) {
+function doAutoFlip (fps) {
     stopFlip()
-    return
-  }
 
-  // Change speed is now frames / second
-  let changeSpeed = 1000 / fps
+  if (fps !== '0') {
+    // Change speed is now frames / second, so calculate our MS interval
+    const changeSpeed = 1000 / fps
 
-  // Only adjust the interval if the frequency has changed
-  if (speed !== changeSpeed) {
-    speed = changeSpeed
-
-    setMusic(changeSpeed)
-    autoFlip();
-    t = setInterval("autoFlip()", speed);
+    setMusic(fps)
+    autoFlip()
+    intervals.push(setInterval(autoFlip, changeSpeed))
   }
 }
 
-function stopFlip() {
-  clearTimeout(t);
-  speed = 0
+function stopFlip () {
+  for (const interval of intervals) {
+    clearTimeout(interval)
+  }
   setMusic(0)
 }
 
-const flipSpeedChange = (event, newSpeed) => {
+// eslint-disable-next-line no-unused-vars
+function flipSpeedChange (event, newSpeed) {
   switch (event) {
     // User is done changing speed, so let's stop the auto flip and restart with new speed
     case 'change':
       doAutoFlip(newSpeed)
+      break
     case 'input':
       // Here we just want to reflect the new speed back to the label while user is dragging the control
       document.getElementById('speed').innerText = newSpeed
@@ -65,7 +63,7 @@ const flipSpeedChange = (event, newSpeed) => {
 }
 
 const setMusic = (newSpeed) => {
-  if (newSpeed == 0) {
+  if (newSpeed === 0) {
     document.getElementById('techno').pause()
     document.getElementById('classical').pause()
   } else if (newSpeed > TECHNO_SPEED) {
